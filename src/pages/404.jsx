d9448efee359a175e62acacc6bb344dd7097c98d
Marie COLLET <marie.collet2023@gmail.com>
2025-06-25 16:31:24 +0200

    Correction affichage 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import "./notfound.css"
 
+import erreur404 from '@/assets/404.webp';
+
 const NotFound = () => {
   return (
     <div className="wrapper notfound-page">
       <h1 className="text-center mb-5">404 - Page non trouvée</h1>
-      <img src="src/assets/404.webp" alt="Page non trouvée" className="img-404" />
+      <img
+        src={erreur404}
+        alt="Page non trouvée"
+        className="img-404"
+      />
 
       <p>Oups ! Cette page n'existe pas.</p>
       <NavLink to="/" className="bouton">Retour à l'accueil</NavLink>
