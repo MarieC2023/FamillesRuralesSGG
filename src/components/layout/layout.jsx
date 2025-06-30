3c2140b43cd5c1edfc63d0b1024a02511973e0c7
Marie COLLET <marie.collet2023@gmail.com>
2025-06-30 10:47:05 +0200

    Ajout scroll to top pour toujours retourner haut de page

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
+import ScrollToTop from './ScrollToTop'
 
 const Layout = () => (
   <>
     <Header />
+    <ScrollToTop />
     <main>
       <Outlet />
     </main>
