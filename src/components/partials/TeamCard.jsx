530f6a873db4bf297afa712350d0237e99f2691b
Marie COLLET <marie.collet2023@gmail.com>
2025-09-02 14:44:40 +0200

    Mise à jour page Team Anim

--- src/components/partials/TeamCard.jsx
+++ src/components/partials/TeamCard.jsx
@@ -10,7 +10,7 @@ function TeamCard({ prenom, fonction, presentation, image, typeImage }) {
       </div>
       {prenom && <h2 className="prenom">{prenom}</h2>}
       <p className="fonction">{fonction}</p>
-      {presentation && <p className="presentation">«&nbsp;{presentation}&nbsp;»</p>}
+      {presentation && <p className="presentation">{presentation}</p>}
     </div>
   );
 }
