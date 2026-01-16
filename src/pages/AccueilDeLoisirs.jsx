4d7d0731232c1c30c24c06a434501d9c88bae025
Marie COLLET <marie.collet2023@gmail.com>
2026-01-16 18:57:22 +0100

    Mise à jour accueil de loisirs vacs février

--- src/pages/AccueilDeLoisirs.jsx
+++ src/pages/AccueilDeLoisirs.jsx
@@ -14,12 +14,12 @@ import entreePeri from '@/assets/entreePeri.webp';
 import intPeri from '@/assets/peri1.webp';
 import extPeri from '@/assets/peri2.webp';
 
-import merc1 from '@/assets/planning/merc1.webp';
+//import merc1 from '@/assets/planning/merc1.webp';
 import merc2 from '@/assets/planning/merc2.webp';
 import vac1 from '@/assets/planning/vac1.webp';
 import vac2 from '@/assets/planning/vac2.webp';
 import vac3 from '@/assets/planning/vac3.webp';
-// import vac4 from '@/assets/planning/vac4.webp';
+import vac4 from '@/assets/planning/vac4.webp';
 
 const Centre = () => {
     return (
@@ -101,11 +101,11 @@ const Centre = () => {
                 <div className="plannings-container d-flex justify-content-center  flex-wrap">
                     <PlanningCarrousel
                         titre="Planning des mercredis"
-                        images={[merc1, merc2]}
+                        images={[merc2]}
                     />
                     <PlanningCarrousel
                         titre="Planning des vacances scolaires"
-                        images={[vac1, vac2, vac3]}
+                        images={[vac1, vac2, vac3, vac4]}
                     />
                 </div>
             </section>
@@ -122,7 +122,7 @@ const Centre = () => {
                         fichiers={[
                             {
                                 href: "/docs/CouponInscription.pdf",
-                                label: "👉 Coupon d'inscription pour les Vacances de décembre 📝",
+                                label: "👉 Coupon d'inscription pour les Vacances de février 📝",
                             },
                             {
                                 href: "/docs/DossierInscription.pdf",
