02913a7eaf4740ecd585b90b2053f70987b1ea2f
Marie COLLET <marie.collet2023@gmail.com>
2026-09-07 18:58:19 +0200

    Mise à jour page centre

--- src/pages/AccueilDeLoisirs.jsx
+++ src/pages/AccueilDeLoisirs.jsx
@@ -15,6 +15,7 @@ import intPeri from '@/assets/peri1.webp';
 import extPeri from '@/assets/peri2.webp';
 
 import merc1 from '@/assets/planning/merc1.webp';
+import date from '@/assets/planning/date.webp';
 //import merc2 from '@/assets/planning/merc2.webp';
 // import vac1 from '@/assets/planning/vac1.webp';
 // import vac2 from '@/assets/planning/vac2.webp';
@@ -104,6 +105,10 @@ const Centre = () => {
                     <PlanningCarrousel
                         titre="Planning des mercredis"
                         images={[merc1]}
+                    />
+                    <PlanningCarrousel
+                        titre="Ouverture vacances scolaires"
+                        images={[date]}
                     />
                     {/* <PlanningCarrousel
                         titre="Planning des vacances scolaires"
